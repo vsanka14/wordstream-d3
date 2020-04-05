7a9ef3cb8f169afed05d890a2a808d1300456ad7
svasisht <vasisht.shankar@gmail.com>
2020-04-04 17:26:47 -0700

    Updates

---
+++ precache-manifest.0e84d34372fa40f03ab854fff3df4a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39c8b36b65aecdc47e499ff37f2c62f",
+    "url": "/wordstream-d3/index.html"
+  },
+  {
+    "revision": "cc1602cd6832b16b629f",
+    "url": "/wordstream-d3/static/css/2.b1712ce1.chunk.css"
+  },
+  {
+    "revision": "f081eb09471cf92d7213",
+    "url": "/wordstream-d3/static/css/main.f68bcc4f.chunk.css"
+  },
+  {
+    "revision": "cc1602cd6832b16b629f",
+    "url": "/wordstream-d3/static/js/2.40a91a7e.chunk.js"
+  },
+  {
+    "revision": "f081eb09471cf92d7213",
+    "url": "/wordstream-d3/static/js/main.e35cadd2.chunk.js"
+  },
+  {
+    "revision": "4b4113faced2cc919824",
+    "url": "/wordstream-d3/static/js/runtime-main.5f1ea537.js"
+  }
+]);
